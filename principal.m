f02b476baeb6045f3dbe87f84a2968eba8c9a867
Heloyza Gois <36577707+heloyza19@users.noreply.github.com>
2018-09-12 13:32:17 -0300

    Add files via upload

--- principal.m
+++ principal.m
@@ -5,15 +5,15 @@ h=10;
 
 %Polígono
 %na=input('número de vértices:');         %número de arestas
-%pt=[1 1; 2 3;2 5; 1 4];
-X=[ 1 2 2 1 ];
-Y=[1 1 2 2];
+pt=[1 1; 2 1;2 2; 1 2];
+X=pt(:,1);
+Y=pt(:,2);
 %[pt,x,y]=poligono(na);                   %coordenadas
 Np=length(X);
 
-
+Pb=cell(Np,2);
 %Número de bolinhas
-Nb=2;
+Nb=10;
 
 X(Np+1)=X(1);
 Y(Np+1)=Y(1);
@@ -24,17 +24,17 @@ for i=1:1:Np
  Raio(i)=L(i)/(2*Nb);
  s=Raio:2*Raio(i):(L-Raio(i));
  for j=1:1:length(s)
-    Xb(i,j)=X(i)+(s(j)/L(i))*(X(i+1)-X(i));
-    Yb(i,j)=Y(i)+(s(j)/L(i))*(Y(i+1)-Y(i));
+    Pb{i,1}(j)=X(i)+(s(j)/L(i))*(X(i+1)-X(i));
+    Pb{i,2}(j)=Y(i)+(s(j)/L(i))*(Y(i+1)-Y(i));
  end
 end
 figure(1)
 teta=0:0.1:2*pi;
 for i=1:1:Np
    for j=1:1:Nb 
-       Xe=Xb(i,j)+cos(teta)*Raio(i);
-       Ye=Yb(i,j)+sin(teta)*Raio(i);
-       plot(Xe,Ye);
+       Xe=Pb{i,1}(j)+cos(teta)*Raio(i);
+       Ye=Pb{i,2}(j)+sin(teta)*Raio(i);
+       plot(Xe,Ye,'r');
        hold on
    end
 end
